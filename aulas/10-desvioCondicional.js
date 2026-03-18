0c2c3f8511652fb9d1222ee2280b837730840a3d
Victor962-hub <vn20012010@gmail.com>
2026-03-18 16:54:39 -0300

    Fizemos varias anotações e atividades com switchs e sistemas de Notas

---
+++ aulas/10-desvioCondicional.js
@@ -0,0 +1,13 @@
+// Desvio Condicional - IF ELSE
+
+/* Condições simples com IF
+Sintaxe: if( condição ){
+    codigo a ser executado, se verdadeiro
+}
+*/
+
+let anoNascimento = 2003
+
+if(anoNascimento > 2003){
+    console.log("A pessoa nasceu depois de 2003.")
+}
