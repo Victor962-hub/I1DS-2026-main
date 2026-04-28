bb73faeaaa0003ad81c2bb98330cebe38336e3c4
Victor962-hub <vn20012010@gmail.com>
2026-04-28 11:31:48 -0300

    Finalizamos a primeira parte do HTML e outros

---
+++ AULAS-HTML/HTML-JAVASCRIPT/js/script.js
@@ -0,0 +1,51 @@
+/*function olaMundo (){
+    alert("Deus Abençoe")
+}*/
+
+/* Forma correta de utilizar o aviso */
+
+const olaMundo = () => {
+  alert("Teste a mudança de texto a baixo!");
+};
+
+const mudarTexto = () => {
+  
+  // Criar uma variavel para armazenar o elemento do documento
+  
+  let elementoAlvo =   document.getElementById("texto");
+    
+  elementoAlvo.innerHTML = " 🚨 Novo texto com JavaScript 🚨";
+    elementoAlvo.style.color = "red";
+    elementoAlvo.style.background = "gray";
+};
+
+ const enviarNome = () =>{
+    let nomeDigitado = document.getElementById("nome").value;
+
+    alert("Olá " + nomeDigitado);
+
+
+
+    /*let nomeDigitado = document.getElementsByName("nome").value;
+
+    alert("Olá " + nomeDigitado)}*/
+ }
+ //Buscou os elementos ID
+ const somar = () => {
+  event.preventDefault(); //
+  // Buscou os elementos por ID
+  let numero1 = document.getElementById("n1").value;
+  let numero2 = parseInt(document.getElementById("n2").value);
+
+  // Realizou a soma dos valores
+  let result = parseInt(numero1) + numero2;
+  // let result =  + numero2;
+
+  // Devolveu o resultado para o formulario
+  document.getElementById("resultado").innerHTML = result;
+};
+
+
+
+    
+ 
