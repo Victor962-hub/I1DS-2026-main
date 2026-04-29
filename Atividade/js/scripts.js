29e9204f9cf730b14dfec1132111b97ced2e5383
Victor962-hub <vn20012010@gmail.com>
2026-04-29 11:27:44 -0300

    .

--- Atividade/js/scripts.js
+++ Atividade/js/scripts.js
@@ -7,10 +7,12 @@ const cadastrar = (event) => {
   // Capturando o elemento de lista por Id
   let lista = document.getElementById("lista");
   // Criar um novo elemento <li>Nome - Email</li>
-  let novoItem = document.createElement("li");
-  novoItem.innerHTML = ${id} 
-    ${nome} - ${email} - <p onClick=editar(${id})class='btn'>Editar</p> <p onClick=excluir(${id}) class='btn'>Excluir</p>';
-    
+
+  userCount++
+  let id = userCount; //Criar um novo elemento <li>Nome - Email</li>
+  let novoItem = document.createElement("li")
+  novoItem.innerHTML = `${id} - ${nome} - ${email} - <p onClick=editar(${id}) class= 'btn' >Editar</p> <p onClick=excluir(${id}) class= 'btn' > Excluir</p>`;
+
 
   // Adicionar o novo item na lista ja existente
   lista.appendChild(novoItem);
@@ -19,7 +21,12 @@ const cadastrar = (event) => {
   document.getElementById("nome").value = "";
   document.getElementById("email").value = "";
 
+  
   //Editar os campos
+  let nome = item.innerHTML.split(" - ")[1];
+  let email = item.innerHTML.split(" - ")[2];
+  
+  
   document.getElementById("nome").value = nome;
   document.getElementById("email").value = email;
 };
@@ -33,7 +40,7 @@ const excluir = (id) => {
   //buscar o conteudo do id na lista
   // e remover o item
   const lista = document.getElementById("lista");
-  const itens = document.getElementById("li");
+  const itens = document.querySelectorAll("li");
 
   itens.forEach((item) => {
     if (item.innerHTML.includes(id)) {
